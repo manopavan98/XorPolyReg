13dd41041e1edf978402aa446f5dbe7c9c805dfa
manopavan98 <55524476+manopavan98@users.noreply.github.com>
2020-08-07 01:03:26 -0500

    Add files via upload

---
+++ polyregxor.m
@@ -0,0 +1,36 @@
+clc;
+close all;
+clear all;
+
+%% inputs and outputs of xor gate 
+x = [0,0;
+    0,1;
+    1,0;
+    1,1];
+y = [0;1;1;0];
+
+%% adding column of ones to X so that it will be in form 1+x, 
+%so when we multiply that with weights it will be in form y =w0 + wx =  w0 + w1*x1 + w2*x2
+%so w0 is bias and w1 and w2 are weights if it is linear regression
+
+in = [ones(length(x),1) x];
+
+%% polynomial regression
+in = [in, x.^2];
+[xs,ys] = size(in);
+
+%taking initial weights 
+w = rand(ys,1);
+ye = in*w;  %Expected Output
+
+%% weights updating
+for n = 1:2000
+    w  = w - 0.02*(in'*(ye-y));
+    ye = in*w;   
+end
+
+
+
+       
+        
+        

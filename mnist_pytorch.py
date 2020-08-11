2616f58eec3d4b4f40e1dd9b8aafef3964232533
manopavan98 <55524476+manopavan98@users.noreply.github.com>
2020-08-11 15:22:48 -0500

    Add files via upload

---
+++ mnist_pytorch.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Aug  11 03:20:13 2020
+
+@author: manop
+"""
+
+import numpy as np
+import torch
+import torchvision
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,)),
+                              ])
+
+trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
+valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
+
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.LogSoftmax(dim=1))
+criterion = nn.NLLLoss()
+images, labels = next(iter(trainloader))
+images = images.view(images.shape[0], -1)
+
+logps = model(images)
+loss = criterion(logps, labels)
+loss.backward()
+
+optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+epochs = 15
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        images = images.view(images.shape[0], -1)
+        optimizer.zero_grad()
+        output = model(images)
+        loss = criterion(output, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+    else:
+        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
+
+images, labels = next(iter(valloader))
+
+img = images[0].view(1, 784)
+with torch.no_grad():
+    logps = model(img)
+
+ps = torch.exp(logps)
+probab = list(ps.numpy()[0])
+
+correct_count, all_count = 0, 0
+for images,labels in valloader:
+  for i in range(len(labels)):
+    img = images[i].view(1, 784)
+    with torch.no_grad():
+        logps = model(img)
+
+    
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    pred_label = probab.index(max(probab))
+    true_label = labels.numpy()[i]
+    if(true_label == pred_label):
+      correct_count += 1
+    all_count += 1
+
+print("Number Of Images Tested =", all_count)
+print("\nModel Accuracy =", (correct_count/all_count))
+
